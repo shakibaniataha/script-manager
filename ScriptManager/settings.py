335ef3a8ca35e6da2e848995c9c2c13833a96b24
Taha Shakibania <shakibaniataha@gmail.com>
2017-12-05 12:43:49 +0330

    Some important configs added (2)

--- ScriptManager/settings.py
+++ ScriptManager/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ScriptManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+
+# CELERY CONFIGS
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
--- main/views.py
+++ main/views.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
