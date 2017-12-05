0e1066be85900a2b45a1e44bcdde42685cf4b053
Taha Shakibania <shakibaniataha@gmail.com>
2017-12-05 18:21:02 +0330

    Added models to admin page. db population code basics added.

--- populate_db.py
+++ populate_db.py
@@ -1,17 +1,36 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ScriptManager.settings')
+
+import django
+django.setup()
+
+from django.contrib.auth.models import User, Group
+from main.models import API, Request
+
+
 def populate():
     pass
 
 
-def add_user():
-    pass
+def add_user(username, password):
+    user = User.objects.create_user(username, password=password)
+    user.save()
+
+    return user
 
 
-def add_group():
-    pass
+def add_group(name):
+    group = Group.objects.get_or_create(name=name)[0]
+    group.save()
+
+    return group
 
 
-def add_user_group():
-    pass
+def add_user_group(group, user):
+    user_group = group.user_set.add(user)
+    user_group.save()
+
+    return user_group
 
 
 def add_api():
